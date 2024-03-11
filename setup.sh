5df3f6ae7d9f3456d7c7991f2e92ade7b1df03f6
Ryan Jackson <rmjackson234@gmail.com>
2024-03-11 16:39:26 -0600

    fixed cronjob hopefully

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ systemctl start cron
 curl -o /root/modbus.py https://raw.githubusercontent.com/RJ-246/byu-dc-pdupoller/main/modbus.py
 curl -o /root/requirements.txt https://raw.githubusercontent.com/RJ-246/byu-dc-pdupoller/main/requirements.txt
 pip3 install -r /root/requirements.txt
-echo "* * * * * python3 /root/modbus.py" | crontab -
+echo "INFLUX_TOKEN=$INFLUX_TOKEN" > /root/crontab.txt
+echo "* * * * * /usr/bin/python3 /root/modbus.py" >> /root/crontab.txt
