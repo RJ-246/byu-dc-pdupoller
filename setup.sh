4e115510ff053c9a02f96ac33bc4a48efcc75224
Ryan Jackson <rmjackson234@gmail.com>
2024-03-11 15:47:49 -0600

    typos

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 apt-get update
 apt-get install systemctl cron python3 python3-pip -y
-systemsctl enable cron
+systemctl enable cron
 systemctl start cron
 curl -o /root/modbus.py https://raw.githubusercontent.com/RJ-246/byu-dc-pdupoller/main/modbus.py
 curl -o /root/requirements.txt https://raw.githubusercontent.com/RJ-246/byu-dc-pdupoller/main/requirements.txt
