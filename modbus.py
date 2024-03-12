15e4a86839f40957af51f87f832585ab972bbfeb
Ryan Jackson <rmjackson234@gmail.com>
2024-03-12 12:36:55 -0600

    added all current PDUs

--- modbus.py
+++ modbus.py
@@ -3,7 +3,8 @@ import influxdb_client, os
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
-token = os.environ['INFLUX_TOKEN']#"G_WAOJWZ1ymjZLUn9L1ZsrpKxYIsUYk42g7AARJgc3CdIT5GEdzLOn9gdT2MYVvIL1k46o_4QV3OQ9KgfP3txQ=="
+#Sets up influxDB info
+token = os.environ['INFLUX_TOKEN']
 influxdb_address = 'pdu_poll-influxdb-1:8086'
 org='byu'
 bucket = 'pdu-data'
@@ -11,40 +12,25 @@ url = f"http://{influxdb_address}"
 
 influx_client = influxdb_client.InfluxDBClient(url=url,token=token,org=org)
 
-pdu_ips = [{'ip': '10.11.82.11', 'name': '1400N_100E_B'}, {'ip': '10.11.82.12', 'name': '1400N 100E C'}, {'ip': '10.11.82.13', 'name': '1400N 200E B'},
+#All IPs for PDUs
+pdu_ips = [{'ip': '10.11.82.11', 'name': '1400N 100E B'}, {'ip': '10.11.82.12', 'name': '1400N 100E C'}, {'ip': '10.11.82.13', 'name': '1400N 200E B'},
            {'ip': '10.11.82.14', 'name': '1400N 200E C'}, {'ip': '10.11.82.15', 'name': '1400N 300E B'}, {'ip': '10.11.82.16', 'name': '1400N 300E C'},
            {'ip': '10.11.82.17', 'name': '1400N 400E B'}, {'ip': '10.11.82.18', 'name': '1400N 400E C'}, {'ip': '10.11.82.19', 'name': '1400N 500E B'},
            {'ip': '10.11.82.20', 'name': '1400N 500E C'}, {'ip': '10.11.82.21', 'name': '1400N 600E B'}, {'ip': '10.11.82.22', 'name': '1400N 600E C'},
-           {'ip': '10.11.82.23', 'name': '1400N 700E B'},{'ip': '10.11.82.24', 'name': '1400N 700E C'}]
+           {'ip': '10.11.82.23', 'name': '1400N 700E B'},{'ip': '10.11.82.24', 'name': '1400N 700E C'},
+           {'ip': '10.11.82.25', 'name': '900N 100E A'}, {'ip': '10.11.82.26', 'name': '900N 100E B'}, {'ip':'10.11.82.27', 'name': '900N 200E A'},
+           {'ip':'10.11.82.28' ,'name': '900N 200E B'}, {'ip': '1011.82.29','name': '900N 300E A'}, {'ip': '10.11.92.30','name': '900N 300E B'},
+           {'ip': '10.11.82.31','name': '900N 400E A'}, {'ip': '10.11.82.32','name':'900N 400E B'}, {'ip': '10.11.82.33','name':'900N 500E A'},
+           {'ip': '10.11.82.34','name':'900N 500E B'}, {'ip': '10.11.82.35','name':'900N 600E B'}, {'ip': '10.11.82.36','name':'900N 600E C'},
+           {'ip': '10.11.82.37','name':'900N 700E A'}, {'ip': '10.11.82.38','name':'900N 700E B'}]
 
+#The registers to read data from (each pdu has a CSV you can download with the register mappings)
 pdu_registers = [{'register': 299, 'mapping': 'Total Real Power', 'units': 'watts'},
                  {'register': 300, 'mapping': 'Total Apparent Power', 'units': 'volt-amps'},
                  {'register': 301, 'mapping': 'Total Power Factor', 'units': '%'},
                  {'register': 302, 'mapping': 'Total Energy', 'units': 'kilowatt-hours'}
                  ]
-device_ip = '10.11.82.24'
 device_port = 502
-
-
-# try:
-#     client = ModbusClient.ModbusTcpClient(device_ip, port=device_port, timeout = 10)
-#     connection = client.connect()
-
-#     if connection:
-#         print('connected')
-#         print(connection)
-#         response = client.read_holding_registers(409, 1, unit=1)
-       
-#         if response.isError():
-#             print(f"Modbus error")
-#             print(response)
-#         else:
-#             print(response.registers[0]/100)
-
-#     client.close()
-#     print('client closed')
-# except Exception as e:
-#     print(f"Error: {e}")
 
 
 for pdu in pdu_ips:
