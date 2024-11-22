de0f783803df876a449c4dc7f1d2519e52358ad2
Ryan Jackson <rmjackson234@gmail.com>
2024-11-22 10:16:34 -0700

    removed export

--- setup.sh
+++ setup.sh
@@ -9,6 +9,6 @@ curl -o /root/modbus.py https://raw.githubusercontent.com/RJ-246/byu-dc-pdupolle
 curl -o /root/requirements.txt https://raw.githubusercontent.com/RJ-246/byu-dc-pdupoller/main/requirements.txt
 /root/venv/bin/pip3 install -r /root/requirements.txt
 chmod +x /root/modbus.py
-export "INFLUX_TOKEN=$ADMIN_TOKEN"
+#export "INFLUX_TOKEN=$ADMIN_TOKEN"
 echo "* * * * * /root/venv/bin/python3 /root/modbus.py" > /root/crontab.txt
 crontab /root/crontab.txt
