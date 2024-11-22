2f95539099080cc1372651fb84f7c2fbe8a97c0c
Ryan Jackson <rmjackson234@gmail.com>
2024-11-22 10:49:52 -0700

    fixed package names

--- setup.sh
+++ setup.sh
@@ -2,12 +2,12 @@
 
 apt-get update
 #apt-get install systemd cron python3 python3-pip python3-venv -y
-apt-get install python3 python3-pip python3-ven watch -y
+apt-get install python3 python3-pip python3-venv watch -y
 python3 -m venv /root/venv
 #systemctl enable cron
 #systemctl start cron
-curl -o /root/modbus.py https://raw.githubusercontent.com/RJ-246/byu-dc-pdupoller/main/modbus.py
-curl -o /root/requirements.txt https://raw.githubusercontent.com/RJ-246/byu-dc-pdupoller/main/requirements.txt
+#curl -o /root/modbus.py https://raw.githubusercontent.com/RJ-246/byu-dc-pdupoller/main/modbus.py
+#curl -o /root/requirements.txt https://raw.githubusercontent.com/RJ-246/byu-dc-pdupoller/main/requirements.txt
 /root/venv/bin/pip3 install -r /root/requirements.txt
 chmod +x /root/modbus.py
 #export "INFLUX_TOKEN=$ADMIN_TOKEN"
